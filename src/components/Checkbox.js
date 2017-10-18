e0be08fffc848ff020f80849f8be27624fcdcc57
David Costa <davidcostadev@gmail.com>
2017-10-18 13:58:01 -0300

    Adicionando o className

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -5,7 +5,7 @@ import '~assets/checkbox.scss'
 
 export default function Checkbox(props) {
   console.log(props)
-  return <div class="checkbox">
+  return <div className="checkbox">
     <input type="checkbox" id={props.id} checked={props.value} onChange={props.onChange}/>
       <label htmlFor={props.id}>{props.label}</label>
     </div>
