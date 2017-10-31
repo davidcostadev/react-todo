fc8ce44ad8bba6a661009bbdaa2159c315e79ae8
David Costa <davidcostadev@gmail.com>
2017-10-31 09:09:30 -0300

    separate responsability

---
+++ src/components/TodoItem.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+import '~assets/todo.scss'
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      task: props.task.name
+    }
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      task: event.target.value
+    })
+  }
+  
+  formEditting() {
+    return (
+      <li
+      className={classNames({
+        'list-group-item': true,
+        'task-item': true,
+        'task-completed': this.props.task.completed,
+      })}
+      key={this.props.task.id}>
+        <input
+          type="text"
+          value={this.state.task}
+          onChange={e => this.handleInputChange(e)} />
+        <button type="button" className="btn btn-save btn-sm btn-outline-success" onClick={() => this.props.onSubmit(this.state.task)}>
+          <i className="material-icons">save</i>
+        </button>
+        <button type="button" className="btn btn-cancel btn-sm btn-outline-danger" onClick={this.props.clearEditing}>
+          <i className="material-icons">clear</i>
+        </button>
+      </li>
+    )  
+  }
+
+  taskDefault() {
+    return (
+      <li
+      className={classNames({
+        'list-group-item': true,
+        'task-item': true,
+        'task-completed': this.props.task.completed,
+      })}
+      key={this.props.task.id}>
+        <Checkbox
+          id={`task-${this.props.task.id}`}
+          label={this.props.task.name}
+          className="task-item-content"
+          value={this.props.task.completed} onChange={this.props.onCompleted} />
+          <button type="button" className="btn btn-edit btn-sm btn-outline-info" onClick={this.props.changeEdit}>
+            <i className="material-icons">edit</i>
+          </button>
+          <button type="button" className="btn btn-delete btn-sm btn-outline-danger" onClick={this.props.onDelete}>
+            <i className="material-icons">delete</i>
+        </button>
+      </li>
+    )
+  }
+
+  render() {
+    if (this.props.editing === this.props.task.id) {
+      return this.formEditting()
+    }
+
+    return this.taskDefault()
+  }
+}
+
+
+export default TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,86 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
-
-import '~assets/todo.scss'
-
 import { toogleTodo, deleteTodo, changeEdit, clearEditing, saveEdit } from '../store/actions'
 
+// Components
 import Checkbox from './Checkbox'
-
-// let editing = null
-
-
-const IfEditing = (props) => {
-  if (props.editing) {
-    return props.children
-  }
-
-  return null
-}
-const IfNotEditing = (props) => {
-  if (!props.editing) {
-    return props.children
-  }
-
-  return null
-}
-
-class TodoItem extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      task: props.task.name
-    }
-  }
-// = ({ form, task, editing, changeEdit, clearEditing, onSubmit, onCompleted, onDelete }) => {
-
-  handleInputChange(event) {
-    console.log(event.target.value)
-    this.setState({
-      task: event.target.value
-    })
-  }
-  
-  render() {
-   
-    return <li
-      className={classNames({
-        'list-group-item': true,
-        'task-item': true,
-        'task-completed': this.props.task.completed,
-      })}
-      key={this.props.task.id}>
-
-        <IfNotEditing editing={this.props.editing === this.props.task.id}>
-          <Checkbox
-          id={`task-${this.props.task.id}`}
-          label={this.props.task.name}
-          className="task-item-content"
-          value={this.props.task.completed} onChange={this.props.onCompleted} />
-          <button type="button" className="btn btn-edit btn-sm btn-outline-info" onClick={this.props.changeEdit}>
-            <i className="material-icons">edit</i>
-          </button>
-          <button type="button" className="btn btn-delete btn-sm btn-outline-danger" onClick={this.props.onDelete}>
-            <i className="material-icons">delete</i>
-          </button>
-        </IfNotEditing>
-        <IfEditing editing={this.props.editing === this.props.task.id}>
-          <input
-            type="text"
-            value={this.state.task}
-            onChange={e => this.handleInputChange(e)} />
-          <button type="button" className="btn btn-save btn-sm btn-outline-success" onClick={() => this.props.onSubmit(this.state.task)}>
-            <i className="material-icons">save</i>
-          </button>
-          <button type="button" className="btn btn-cancel btn-sm btn-outline-danger" onClick={this.props.clearEditing}>
-            <i className="material-icons">clear</i>
-          </button>
-        </IfEditing>  
-    </li>
-  }
-}
+import TodoItem from './TodoItem'
 
 
 const TodoNode = ({ todo, editing, clearEditing, saveEdit, changeEdit, onCompletedTodo, onDeleteTodo }) => {
@@ -105,6 +30,8 @@ const TodoList = (payload) => {
 }
 
 
+// Maps
+
 const mapStateTodos = state => {
   return state
 }
@@ -121,7 +48,6 @@ const mapStateChange = dispatch => {
     clearEditing: id => dispatch(clearEditing(id)),
   }
 }
-
 
 const TodoListConnect = connect(
   mapStateTodos,
